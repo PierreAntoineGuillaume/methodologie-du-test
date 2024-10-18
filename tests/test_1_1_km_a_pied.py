28402727f4646f391959b31f2ffd677adfb83b57
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2024-10-18 12:27:33 +0200

    test: ajout fonction

--- tests/test_1_1_km_a_pied.py
+++ tests/test_1_1_km_a_pied.py
@@ -1,4 +1,8 @@
 import pytest
 
+def premier_paragraphe():
+    return "1 kilomètre à pied, ça use, ça use,\n1 kilomètre à pied, ça use les souliers."
+
+
 def test_premier_paragraphe():
     assert "1 kilomètre à pied, ça use, ça use,\n1 kilomètre à pied, ça use les souliers." == premier_paragraphe()
