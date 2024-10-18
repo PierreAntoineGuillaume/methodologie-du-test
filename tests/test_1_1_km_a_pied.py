51c6d2d8c9f5d352a8639077906eb283b714d101
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2024-10-18 14:25:40 +0200

    test: glue function tested

--- tests/test_1_1_km_a_pied.py
+++ tests/test_1_1_km_a_pied.py
@@ -1,6 +1,5 @@
 import pytest
 
-
 def premier_paragraphe():
     return (
         "1 kilomètre à pied, ça use, ça use,\n1 kilomètre à pied, ça use les souliers."
@@ -23,3 +22,27 @@ def test_generer_chanson():
         "5 kilomètres à pied, ça use, ça use,\n5 kilomètres à pied, ça use les souliers."
         == generer_paragraphe(5)
     )
+
+def test_glu():
+    assert (
+            (
+                "1 kilomètre à pied, ça use, ça use,\n1 kilomètre à pied, ça use les souliers.\n"
+                "2 kilomètres à pied, ça use, ça use,\n2 kilomètres à pied, ça use les souliers.\n"
+                "3 kilomètres à pied, ça use, ça use,\n3 kilomètres à pied, ça use les souliers."
+
+            ) == glu_paragraphe(3)
+    )
+def glu_paragraphe(n):
+    result = ''
+    for i in range (1 , n+1):
+
+        if i == 1 :
+
+            result = premier_paragraphe()
+        if i > 1 :
+
+            result += '\n' + generer_paragraphe(i)
+
+    return result
+
+
