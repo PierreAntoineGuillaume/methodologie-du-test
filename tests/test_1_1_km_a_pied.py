9230ad3c6bfe30f7fa95d36daaad940770a8efed
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2024-10-18 13:53:44 +0200

    test: oui

--- tests/test_1_1_km_a_pied.py
+++ tests/test_1_1_km_a_pied.py
@@ -3,6 +3,8 @@ import pytest
 def premier_paragraphe():
     return "1 kilomètre à pied, ça use, ça use,\n1 kilomètre à pied, ça use les souliers."
 
+def generer_paragraphe(kilometres):
+    return f"{kilometres} kilomètres à pied, ça use, ça use,\n{kilometres} kilomètres à pied, ça use les souliers."
 
 def test_premier_paragraphe():
     assert "1 kilomètre à pied, ça use, ça use,\n1 kilomètre à pied, ça use les souliers." == premier_paragraphe()
