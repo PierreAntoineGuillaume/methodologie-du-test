8e7c461dcd9deb1898210a53da8ee7eb8161556e
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2024-10-18 14:26:00 +0200

    refactor: reformat

--- tests/test_1_1_km_a_pied.py
+++ tests/test_1_1_km_a_pied.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def premier_paragraphe():
     return (
         "1 kilomètre à pied, ça use, ça use,\n1 kilomètre à pied, ça use les souliers."
@@ -23,26 +24,21 @@ def test_generer_chanson():
         == generer_paragraphe(5)
     )
 
+
 def test_glu():
     assert (
-            (
-                "1 kilomètre à pied, ça use, ça use,\n1 kilomètre à pied, ça use les souliers.\n"
-                "2 kilomètres à pied, ça use, ça use,\n2 kilomètres à pied, ça use les souliers.\n"
-                "3 kilomètres à pied, ça use, ça use,\n3 kilomètres à pied, ça use les souliers."
+        "1 kilomètre à pied, ça use, ça use,\n1 kilomètre à pied, ça use les souliers.\n"
+        "2 kilomètres à pied, ça use, ça use,\n2 kilomètres à pied, ça use les souliers.\n"
+        "3 kilomètres à pied, ça use, ça use,\n3 kilomètres à pied, ça use les souliers."
+    ) == glu_paragraphe(3)
 
-            ) == glu_paragraphe(3)
-    )
+
 def glu_paragraphe(n):
-    result = ''
-    for i in range (1 , n+1):
-
-        if i == 1 :
-
+    result = ""
+    for i in range(1, n + 1):
+        if i == 1:
             result = premier_paragraphe()
-        if i > 1 :
-
-            result += '\n' + generer_paragraphe(i)
+        if i > 1:
+            result += "\n" + generer_paragraphe(i)
 
     return result
-
-
