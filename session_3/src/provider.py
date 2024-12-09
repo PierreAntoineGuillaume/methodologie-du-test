3344938a6775d7e6e79e52aa589aeefd5d998551
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2024-12-09 10:34:48 +0100

    feat: provider tests pass

--- session_3/src/provider.py
+++ session_3/src/provider.py
@@ -1,6 +1,14 @@
+import csv
 from typing import Any
 
 
 class Provider:
+    def __init__(self) -> None:
+        pass
     def get(self) -> list[Any]:
-        return []
+        with open("transactions-fair.csv", "r") as file:
+            reader = csv.DictReader(file)
+            data = []
+            for row in reader:
+                data.append(row)
+        return data
