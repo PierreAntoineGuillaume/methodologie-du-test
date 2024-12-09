2537a3730362a3084da552913ce7f7d82aaa5317
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2024-12-09 11:48:03 +0100

    feat: clean prices

--- session_3/src/pipeline.py
+++ session_3/src/pipeline.py
@@ -3,6 +3,17 @@ import pandas as pd
 
 class Cleaner:
     def clean(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.drop(
+            labels=["date", "transaction_id", "client_id", "product_id"], axis=1
+        )
+        for i in range(len(df)):
+            if df["amount"][i] is None:
+                continue
+            if not df["amount"][i].isnumeric():
+                df["amount"][i] = None
+                continue
+            if int(df["amount"][i]) < 1:
+                df["amount"][i] = None
         return df.dropna(axis=0)
 
 
--- session_3/tests/test_provider.py
+++ session_3/tests/test_provider.py
@@ -6,10 +6,10 @@ from pipeline import Cleaner
 def test_clean_dataframe() -> None:
     df = pd.DataFrame(
         {
-            "transaction_id": [None],
+            "transaction_id": ["1"],
             "client_id": ["1"],
             "product_id": ["1"],
-            "amount": ["25"],
+            "amount": [None],
             "date": ["2024-01-01"],
             "label": ["legit"],
         }
@@ -18,3 +18,37 @@ def test_clean_dataframe() -> None:
     df = cleaner.clean(df)
     print(df)
     assert df.empty
+
+
+def test_clean_dataframe_keeps_legitimate_amounts() -> None:
+    fake_data = range(0, 5)
+    df = pd.DataFrame(
+        {
+            "transaction_id": fake_data,
+            "client_id": fake_data,
+            "product_id": fake_data,
+            "date": fake_data,
+            "amount": ["1", "-1", "bonjour", "", None],
+            "label": ["legit", "legit", "legit", "legit", "legit"],
+        }
+    )
+    cleaner = Cleaner()
+    df = cleaner.clean(df)
+    assert df["amount"].to_list() == ["1"]
+
+
+def test_clean_dataframe_trims_nonessential_data() -> None:
+    date = "2024-11-02T18:51:23.988846"
+    df = pd.DataFrame(
+        {
+            "transaction_id": [None, "1", "1", "1"],
+            "client_id": ["1", None, "1", "1"],
+            "product_id": ["1", "1", None, "1"],
+            "date": [date, date, date, None],
+            "amount": ["25", "25", "25", "25"],
+            "label": ["legit", "legit", "legit", "legit"],
+        }
+    )
+    cleaner = Cleaner()
+    df = cleaner.clean(df)
+    assert df.shape[0] == 4
