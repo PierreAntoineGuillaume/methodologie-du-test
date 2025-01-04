4377fbdf92c485e77b1444ce7ad357f67c9348f0
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2025-01-04 23:24:28 +0100

    chore: test completer une tache

--- session_2/tests/test_tasks.py
+++ session_2/tests/test_tasks.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.tasks import app
 
 
@@ -22,8 +23,8 @@ def test_url(client):
     assert response.status_code == 404
 
 
-def test_add_task(client):
-    response = client.post(
+def ajouter_tache(client):
+    return client.post(
         "/tasks",
         json={
             "title": "Preparer le rapport",
@@ -33,6 +34,14 @@ def test_add_task(client):
             "difficulty": 2,
         },
     )
+
+
+def ajouter_tache_recuperer_id(client):
+    return ajouter_tache(client).get_json()['id']
+
+
+def test_add_task(client) -> None:
+    response = ajouter_tache(client)
     assert response.status_code == 201
     keys = response.get_json()
     assert keys["description"] == "Finaliser le rapport pour la reunion"
@@ -41,3 +50,13 @@ def test_add_task(client):
     assert keys["priority"] == 3
     assert keys["title"] == "Preparer le rapport"
     assert keys["worth"] == 60
+
+
+def test_clean_task(client) -> None:
+    task_id = ajouter_tache_recuperer_id(client)
+    response = client.post(f"/tasks/{task_id}/complete")
+    assert response.status_code == 200
+    assert response.get_json() == {
+        'message': 'Tâche marquée comme terminée',
+        'score_added': 60,
+    }
